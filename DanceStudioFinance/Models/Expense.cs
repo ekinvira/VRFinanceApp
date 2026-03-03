b3b7da8c9e761446845f514f3c410cbefe052d17
ekinv <ekinv@DESKTOP-OGQ85FC>
2026-03-03 10:51:03 +0300

    Add project files.

---
+++ DanceStudioFinance/Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DanceStudioFinance.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Today;
+
+        // Kira, Elektrik, Maaş vs.
+        public string Category { get; set; } = "";
+
+        public decimal Amount { get; set; }
+
+        public string? Note { get; set; }
+    }
+}
